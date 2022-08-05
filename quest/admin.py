276f5d634790fc84da4387b62e81b5fcc3766986
Harishkumar <harishkumarsp023@gmail.com>
2022-08-05 14:09:18 +0530

    added the bashbord and ceo and manager authication and also added admin control

--- quest/admin.py
+++ quest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ceo
+from .models import manager
 # Register your models here.
+admin.site.register(ceo)
+admin .site.register(manager)
--- quest/views.py
+++ quest/views.py
@@ -1,5 +1,6 @@
 import django
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from quest.models import ceo
 from quest.models import manager
 from django.contrib import messages
@@ -13,13 +14,32 @@ def home(request):
         userdel =  ceo.objects.get(email=request.POST['email'],password=request.POST['password'])
         print("username=",userdel)
         request.session['email']=userdel.email
-        return render(request,"ceo.html",messages.success(request,'welcome '+ request.POST['email']))
+        return render(request,"dashboard/main.html",messages.success(request,'COMPANY CEO'))
        except  ceo.DoesNotExist as e:
          messages.success(request,'username / password invaid..!')  
   return render(request,'index.html') 
- 
- 
+def man(request):
+  if request.method == 'POST':
+       try:
+        userdel =  manager.objects.get(email=request.POST['email'],password=request.POST['password'])
+        print("username=",userdel)
+        request.session['email']=userdel.email
+        
+        return render(request,"dashboard/main.html",messages.success(request, 'Manager'))
+       except  manager.DoesNotExist as e:
+         messages.success(request,'username / password invaid..!')  
+  return render(request,'manager.html') 
+def logout(request):
+  
+  return redirect(request,'logout.html')  
+
+
+  
+  
+
   
 
 def main(request):
   return render(request,'index.html')   
+def dash(request):
+  return render(request , 'dashboard/main.html')  
